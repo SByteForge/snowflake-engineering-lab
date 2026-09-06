7b8b6d77f68709141252af434377e3360501159f
SBtyeForge <knowledgevillage1093@gmail.com>
2026-09-06 10:33:21 +0530

    add CI CD with GCP OIDC and remote Terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,11 @@ terraform {
       source = "snowflakedb/snowflake"
     }
   }
+
+  backend "gcs" {
+    bucket = "snowflake-tf-state-shubham-2026"
+    prefix = "snowflake-engineering-lab/state"
+  }
 }
 
 provider "snowflake" {
